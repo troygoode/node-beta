70134b0e528879cc61aae38a16484fc13c4cf0fb
Troy Goode <troygoode@gmail.com>
2012-09-19 17:26:11 -0400

    added support for account lookups

--- test/envvar_store.js
+++ test/envvar_store.js
@@ -10,6 +10,11 @@ describe('EnvVarStore', function(){
   process.env.BETAFLAG_SOMEFLAG5 = 'troy,bob';
   process.env.BETAFLAG_SOMEFLAG6 = 'bob,troy,alice';
   process.env.BETAFLAG_SOMEFLAG7 = 'bob,alice';
+  process.env.BETAFLAG_SOMEFLAG8 = 'acme:troy';
+  process.env.BETAFLAG_SOMEFLAG9 = 'acme:*';
+  process.env.BETAFLAG_SOMEFLAG10 = 'acme:bob';
+  process.env.BETAFLAG_SOMEFLAG11 = 'enron:troy';
+  process.env.BETAFLAG_SOMEFLAG12 = 'enron:*';
   var store = Store({});
   var account = null;
   var user = null;
@@ -51,6 +56,19 @@ describe('EnvVarStore', function(){
       flags.SOMEFLAG5.should.equal(true);
       flags.SOMEFLAG6.should.equal(true);
       flags.SOMEFLAG7.should.equal(false);
+      done();
+    });
+  });
+
+  it('Allows lookups by account+user.', function(done){
+    //act
+    store(['SOMEFLAG8', 'SOMEFLAG9', 'SOMEFLAG10', 'SOMEFLAG11', 'SOMEFLAG12'], 'acme', 'troy', function(err, flags){
+      //assert
+      flags.SOMEFLAG8.should.equal(true);
+      flags.SOMEFLAG9.should.equal(true);
+      flags.SOMEFLAG10.should.equal(false);
+      flags.SOMEFLAG11.should.equal(false);
+      flags.SOMEFLAG12.should.equal(false);
       done();
     });
   });
