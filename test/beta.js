490bdeeaf1d3dc371cfddf3545aa326d56808c59
Troy Goode <troygoode@gmail.com>
2012-09-19 14:24:49 -0400

    now passing req through middleware

--- test/beta.js
+++ test/beta.js
@@ -28,6 +28,35 @@ describe('Beta with custom strategy', function(){
   });
 });
 
+describe('Beta middleware', function(){
+  //arrange
+  var requestReceived = false;
+  var strategy = function(flag, req, cb){
+    if(req) requestReceived = true;
+    cb();
+  };
+  var beta = new Beta({strategy: strategy});
+  var req = {};
+
+  //act
+  beta.middleware(req, null, function(){
+    it('should add a function to the request object.', function(done){
+      //assert
+      req.beta.should.be.a('function');
+      done();
+    });
+
+    it('should pass the request object into the strategy automatically.', function(done){
+      //act
+      req.beta('SOMEFLAG', function(err, on){
+        //assert
+        requestReceived.should.equal(true);
+        done();
+      });
+    });
+  });
+});
+
 describe('Beta with default strategy', function(){
   //arrange
   var beta = new Beta();
